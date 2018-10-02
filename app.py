f8e76cf707acc7510d27cb98ca5117479afa1292
Roger Humdinger <38846264+mcpa0003@users.noreply.github.com>
2018-10-02 00:14:02 -0500

    took out the try/except

--- app.py
+++ app.py
@@ -34,29 +34,29 @@ def update_twitter():
     mentions = api.search(q="@MichaelMcPart10 Analyze:")
     print(mentions)
     words = []
-    try:
-        command = mentions["statuses"][0]["text"]
-        words = command.split("Analyze:")
-        target_account = words[1].strip()
-        print(f"analysis for target_account: {target_account}")
-        user_tweets = api.user_timeline(target_account, page=1)
+
+    command = mentions["statuses"][0]["text"]
+    words = command.split("Analyze:")
+    target_account = words[1].strip()
+    print(f"analysis for target_account: {target_account}")
+    user_tweets = api.user_timeline(target_account, page=1)
         
-        sentiments = []
+    sentiments = []
 
         # Loop through tweets
-        for tweet in user_tweets:
+    for tweet in user_tweets:
                                 # Run Vader Analysis on each tweet
-            results = analyzer.polarity_scores(tweet["text"])
-            compound = results["compound"]
-            pos = results["pos"]
-            neu = results["neu"]
-            neg = results["neg"]
+        results = analyzer.polarity_scores(tweet["text"])
+        compound = results["compound"]
+        pos = results["pos"]
+        neu = results["neu"]
+        neg = results["neg"]
         
         # Get Tweet ID, subtract 1, and assign to oldest_tweet
        ########### oldest_tweet = tweet['id'] - 1
         
         # Add sentiments for each tweet into a list
-            sentiments.append({"Date": tweet["created_at"], 
+        sentiments.append({"Date": tweet["created_at"], 
                            "Compound": compound,
                            "Positive": pos,
                            "Negative": neu,
@@ -64,32 +64,30 @@ def update_twitter():
                            "Tweets Ago": counter})
                                  
                 # Convert sentiments to DataFrame
-            sentiments_pd = pd.DataFrame.from_dict(sentiments)
+        sentiments_pd = pd.DataFrame.from_dict(sentiments)
     # Create plot
-            plt.figure(figsize=(6, 4), dpi=300)
-            x_vals = sentiments_pd["Tweets Ago"]
-            y_vals = sentiments_pd["Compound"]
-            plt.plot(x_vals,
+        plt.figure(figsize=(6, 4), dpi=300)
+        x_vals = sentiments_pd["Tweets Ago"]
+        y_vals = sentiments_pd["Compound"]
+        plt.plot(x_vals,
                  y_vals, marker="o", linewidth=0.3,
                  alpha=0.8)
 #plt.figure(figsize=(6, 4), dpi=300)
 
 # # Incorporate the other graph properties
-            now = datetime.now()
-            now = now.strftime("%Y-%m-%d %H:%M")
-            plt.title(f"Sentiment Analysis of Tweets ({now}) for {target_user}")
-            plt.xlim([x_vals.max(),x_vals.min()]) #Bonus
-            plt.ylabel("Tweet Polarity")
-            plt.xlabel("Tweets Ago")
-            plt.figure(figsize=(6, 4), dpi=300)
-#####            fig = bar.get_figure()
-            plt.savefig("plot.png")
-            api.update_with_media(
+        now = datetime.now()
+        now = now.strftime("%Y-%m-%d %H:%M")
+        plt.title(f"Sentiment Analysis of Tweets ({now}) for {target_user}")
+        plt.xlim([x_vals.max(),x_vals.min()]) #Bonus
+        plt.ylabel("Tweet Polarity")
+        plt.xlabel("Tweets Ago")
+        plt.figure(figsize=(6, 4), dpi=300)
+        plt.savefig("plot.png")
+        api.update_with_media(
                 "plot.png", "Vader Sentiment Analysis for " + target_account
 #            except Exception:
 #        raise
-    except (KeyError, IndexError):
-        print("Key or Index error")
+
 
 
     # Grab Self Tweets
